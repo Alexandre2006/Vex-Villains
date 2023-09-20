8f4987df74937f8b3ec525cc560f2fc0c75a3136
Alexandre Haddad-Delaveau <ahaddaddelaveau@icloud.com>
2023-09-20 16:51:03 -0700

    Created Vex V5 Project

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       thinkalex                                                    #
+# 	Created:      9/20/2023, 4:48:58 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
